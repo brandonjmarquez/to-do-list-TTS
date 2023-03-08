96a9a2a419fe33ed8679576c73ca087e8e870417
brandon <60440397+brandonjmarquez@users.noreply.github.com>
2023-03-07 18:24:36 -0600

    Started markAsComplete function.

--- todolist.js
+++ todolist.js
@@ -1,27 +1,30 @@
-function addRow(table, taskText, categText) {
-  let row = document.createElement("tr")
-  let completed = document.createElement("td")
-  let completedCheckbox = document.createElement("input")
-  let task = document.createElement("td")
-  let category = document.createElement("td")
+function addRow(table, taskText, categoryText) {
+  let row = document.createElement('tr')
+  let completed = document.createElement('td')
+  let completedCheckbox = document.createElement('input')
+  let task = document.createElement('td')
+  let category = document.createElement('td')
 
-  completedCheckbox.type = "checkbox";
+  completedCheckbox.type = 'checkbox';
   task.innerText = taskText;
-  category.innerText = categText;
+  category.innerText = categoryText;
   completed.appendChild(completedCheckbox);
+  // task.classList.add('todo-text');
+  // category.classList.add('todo-text');
+  row.classList.add('todo-row');
   row.appendChild(completed);
   row.appendChild(task);
   row.appendChild(category);
-  table.appendChild(row)
+  table.querySelector('tbody').appendChild(row)
 }
 
 function addToDo() {
   let taskText = document.getElementById('add-task').value;
-  let categText = document.getElementById('add-category').value;
+  let categoryText = document.getElementById('add-category').value;
   let table = document.getElementById('todo-table');
   
-  if(categText.length !== 0 && taskText.length !== 0)
-    addRow(table, taskText, categText);
+  if(categoryText.length !== 0 && taskText.length !== 0)
+    addRow(table, taskText, categoryText);
 
   document.getElementById('add-task').value = ''
   document.getElementById('add-category').value = '';
@@ -32,7 +35,7 @@ function removeRow(e) {
   let rowParent = e.target.parentElement;
   let toDos = rowParent.parentElement;
 
-  if(rowParent.tagName === "TR" && rowParent.id !== 'labels' && rowParent.id !== 'inputs') {
+  if(rowParent.tagName === 'TR' && rowParent.id !== 'labels' && rowParent.id !== 'inputs') {
     if(neverDeleted) {
       if(confirm('Double clicking a row deletes.\nProceed?')) {
         neverDeleted = false;
@@ -44,8 +47,12 @@ function removeRow(e) {
   }
 }
 
-function markAsComplete() {
-
+function markAsComplete(e) {
+  console.log(e.target.type)
+  if(e.target.tagName === 'INPUT' && e.target.type === 'checkbox') {
+    console.log(e.target.type)
+    
+  }
 }
 
 let neverDeleted = true;
@@ -53,4 +60,5 @@ let neverDeleted = true;
 let addButton = document.getElementById('add-button');
 addButton.addEventListener('click', addToDo);
 let table = document.getElementById('todo-table');
-table.addEventListener("dblclick", removeRow)
+table.addEventListener('dblclick', removeRow)
+table.addEventListener('click', markAsComplete)
